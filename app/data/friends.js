ab4100e5bbd8d685dfcdf3f89775e20b3b351ed8
Justin Igawa <J_Igawa@Justins-MacBook-Air.local>
2016-11-30 16:22:31 -0800

    changed friends info

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [{
-    "name": "Brandon",
-    "photo": "",
+    "name": "D'Angelo Russell",
+    "photo": "http://media.nbclosangeles.com/images/1200*677/Lakers-start-D-Angelo-Russell-2015-16-opener.jpg",
     "scores": [
         "2",
         "3",
@@ -14,8 +14,8 @@ var friends = [{
         "5"
     ]
 }, {
-    "name": "Corey",
-    "photo": "",
+    "name": "Nick Young",
+    "photo": "http://lakerspulse.com/wp-content/uploads/2016/08/NIck-young-3.jpg",
     "scores": [
         "5",
         "5",
@@ -29,8 +29,8 @@ var friends = [{
         "3"
     ]
 }, {
-    "name": "Daniel",
-    "photo": "",
+    "name": "Brandon Ingram",
+    "photo": "http://cdn.fansided.com/wp-content/blogs.dir/307/files/2016/09/9567726-brandon-ingram-nba-los-angeles-lakers-media-day-2-850x560.jpg",
     "scores": [
         "5",
         "4",
@@ -44,34 +44,34 @@ var friends = [{
         "4"
     ]
 }, {
-    "name": "Stephanie",
-    "photo": "",
+    "name": "Metta World Peace",
+    "photo": "http://p.fod4.com/p/channels/pbjmi/profile/PKKGrsvlTKGsN2wAVbCq_MWP.jpg",
     "scores": [
         "5",
         "5",
         "1",
+        "1",
         "5",
         "5",
         "5",
-        "3",
-        "3",
         "5",
-        "4"
+        "5",
+        "5"
     ]
 }, {
-    "name": "Lynzie",
-    "photo": "",
+    "name": "Julius Randle",
+    "photo": "http://sports.cbsimg.net/images/visual/whatshot/julius_randle_040116.jpg",
     "scores": [
         "5",
+        "1",
+        "1",
+        "4",
+        "2",
         "5",
-        "3",
-        "4",
-        "4",
-        "3",
-        "3",
+        "1",
         "5",
-        "4",
-        "5"
+        "2",
+        "2"
     ]
 } ]
 
